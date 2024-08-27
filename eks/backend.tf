541904d45e4fffd3facf9193a53d97c86208917b
Tuhan94 <131899487+Tuhan94@users.noreply.github.com>
2024-08-26 20:30:37 -0400

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-ews-baket-1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "lock-files"
     encrypt        = true
   }
 }
